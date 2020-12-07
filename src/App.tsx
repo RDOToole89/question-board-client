b3492600df783186d132230f4510e8daf6e0015b
thibaudszy <thivaudszy@gmail.com>
2020-12-07 17:04:32 +0100

    :sparkles: Adds navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import AppNavbar from "./Components/Navbar/AppNavbar";
 import Login from "./Pages/Login/Login";
 
 function App() {
   return (
     <div className="App">
+      <AppNavbar />
       question board app
       <Switch>
         {/* <Route exact path="/" component={HomePage} /> */}
