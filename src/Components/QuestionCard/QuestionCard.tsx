1c27f97b1f974c64ceca8e5c35b4ef091c959126
RDOToole89 <roibinotoole@gmail.com>
2020-12-08 13:45:59 +0100

    Create TagBox component -static

--- src/Components/QuestionCard/QuestionCard.tsx
+++ src/Components/QuestionCard/QuestionCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
+import TagBox from "../TagBox/TagBox";
 
 interface cardProps {
   title: string;
@@ -12,11 +13,13 @@ function QuestionCard(props: cardProps) {
   const { title, body, resolved, upVotes } = props;
 
   return (
-    <Card style={{ width: "18rem" }}>
+    <Card className="mb-4" style={{ width: "28rem" }}>
       {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
       <Card.Body>
         <Card.Title>{title}</Card.Title>
-        <Card.Text>{body}</Card.Text>
+        <Card.Subtitle className="mb-2">Roibin O'Toole</Card.Subtitle>
+        <Card.Text className="mb-4">{`${body.slice(0, 100)}...`}</Card.Text>
+        <TagBox />
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
