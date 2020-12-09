8a989c29efdbcdec4e0058dfa3d7248002af8e31
RDOToole89 <roibinotoole@gmail.com>
2020-12-09 23:36:44 +0100

    Add BUGGY comment section with Sockets.io

--- src/store/questions/actions.ts
+++ src/store/questions/actions.ts
@@ -4,49 +4,57 @@ import { AppThunk } from '../types';
 import { selectToken } from '../user/selectors';
 export const SET_QUEUE = 'SET_QUEUE';
 export const SAVE_QUESTION = 'SAVE_QUESTION';
+export const SAVE_COMMENT = 'SAVE_COMMENT';
 
 export const saveQuestion = (question: {}) => {
-	return {
-		type: SAVE_QUESTION,
-		payload: { ...question }
-	};
+  return {
+    type: SAVE_QUESTION,
+    payload: { ...question },
+  };
+};
+
+export const saveComment = (comment: {}) => {
+  console.log(comment);
+  return {
+    type: SAVE_COMMENT,
+    payload: { ...comment },
+  };
 };
 
 export const getQueue = (): AppThunk => async (dispatch, getState) => {
-	const serverResponse = await Axios.get(`${apiUrl}/questions/unresolved`);
-	dispatch({
-		type: SET_QUEUE,
-		payload: serverResponse.data
-	});
+  const serverResponse = await Axios.get(`${apiUrl}/questions/unresolved`);
+  dispatch({
+    type: SET_QUEUE,
+    payload: serverResponse.data,
+  });
 };
 
 export const getQuestion = (questionId: number): AppThunk => async (dispatch, getState) => {
-	try {
-		const response = await Axios.get(`${apiUrl}/questions/${questionId}`);
+  try {
+    const response = await Axios.get(`${apiUrl}/questions/${questionId}`);
 
-		if (response) {
-			console.log(response);
-			dispatch(saveQuestion(response.data));
-		}
-	} catch (e) {
-		console.log(e);
-	}
+    if (response) {
+      dispatch(saveQuestion(response.data));
+    }
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 export const updateQuestion = (questionId: number, key: string, newValue: any): AppThunk => {
-	return async (dispatch, getState) => {
-		const token = selectToken(getState());
-		const serverResponse = await Axios.put(
-			`${apiUrl}/questions/${questionId}`,
-			{
-				key,
-				newValue
-			},
-			{
-				headers: { Authorization: `Bearer ${token}` }
-			}
-		);
-		console.log(serverResponse);
-		dispatch(getQueue());
-	};
+  return async (dispatch, getState) => {
+    const token = selectToken(getState());
+    const serverResponse = await Axios.put(
+      `${apiUrl}/questions/${questionId}`,
+      {
+        key,
+        newValue,
+      },
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    console.log(serverResponse);
+    dispatch(getQueue());
+  };
 };
--- src/store/questions/reducer.ts
+++ src/store/questions/reducer.ts
@@ -1,28 +1,34 @@
-import { SAVE_QUESTION, SET_QUEUE } from './actions';
+import { SAVE_COMMENT, SAVE_QUESTION, SET_QUEUE } from './actions';
 
 interface QuestionState {
-	queue: Question[];
-	single: {};
+  queue: Question[];
+  single: { author: {}; comments: []; tags: [] };
 }
 
 const initialState: QuestionState = {
-	queue: [],
-	single: { author: {}, comments: [], tags: [] }
+  queue: [],
+  single: { author: {}, comments: [], tags: [] },
 };
 // eslint-disable-next-line
 export default (state = initialState, { type, payload }: Action) => {
-	switch (type) {
-		case SET_QUEUE:
-			return { ...state, queue: [ ...payload ] };
+  switch (type) {
+    case SET_QUEUE:
+      return { ...state, queue: [...payload] };
 
-		case SAVE_QUESTION: {
-			return {
-				...state,
-				single: { ...payload }
-			};
-		}
+    case SAVE_QUESTION: {
+      return {
+        ...state,
+        single: { ...payload },
+      };
+    }
+    case SAVE_COMMENT: {
+      return {
+        ...state,
+        single: { ...state.single, comments: [...state.single.comments, { ...payload }] },
+      };
+    }
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 };
