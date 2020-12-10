b7beabb7bc3b1cc9e84bfdef94699113fdb26dcc
thibaudszy <thivaudszy@gmail.com>
2020-12-10 14:32:20 +0100

    Adjustments to the width of elements (CSS)

--- src/Pages/QuestionDetails/QuestionDetails.tsx
+++ src/Pages/QuestionDetails/QuestionDetails.tsx
@@ -1,17 +1,20 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory, useParams } from 'react-router-dom';
-import TagBox from '../../Components/TagBox/TagBox';
-import UpVotes from '../../Components/UpVotes/UpVotes';
-import { getQuestion, saveComment } from '../../store/questions/actions';
-import { selectQuestion, selectSortedComments } from '../../store/questions/selectors';
-import moment from 'moment';
-import './QuestionDetails.css';
-import io from 'socket.io-client';
-import { apiUrl } from '../../config/constants';
-import { Button, Col, Form, FormControl, InputGroup } from 'react-bootstrap';
-import { selectToken, selectUser } from '../../store/user/selectors';
-import UpVotesComments from '../../Components/UpVotesComments/UpVotesComments';
+import React, { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useParams } from "react-router-dom";
+import TagBox from "../../Components/TagBox/TagBox";
+import UpVotes from "../../Components/UpVotes/UpVotes";
+import { getQuestion, saveComment } from "../../store/questions/actions";
+import {
+  selectQuestion,
+  selectSortedComments,
+} from "../../store/questions/selectors";
+import moment from "moment";
+import "./QuestionDetails.css";
+import io from "socket.io-client";
+import { apiUrl } from "../../config/constants";
+import { Button, FormControl, InputGroup } from "react-bootstrap";
+import { selectToken, selectUser } from "../../store/user/selectors";
+import UpVotesComments from "../../Components/UpVotesComments/UpVotesComments";
 
 interface Params {
   id: string;
@@ -23,7 +26,6 @@ interface Author {
 }
 
 interface Comment {
-
   id: number;
   questionId: number;
   body: string;
@@ -47,17 +49,16 @@ function QuestionDetails() {
   const [socketId, setSocketId] = useState(0);
   // @ts-ignore
   const [comment, setComment] = useState<Comment>({
-
     questionId: 0,
-    body: '',
+    body: "",
     authorId: user.id,
     upVotes: 0,
     isSolution: null,
-    author: { firstName: '', lastName: '' },
-    createdAt: moment(new Date()).format('YYYY-MM-DD, h:mm:ss a'),
+    author: { firstName: "", lastName: "" },
+    createdAt: moment(new Date()).format("YYYY-MM-DD, h:mm:ss a"),
   });
 
-  console.log('COMMENT', comment);
+  console.log("COMMENT", comment);
 
   console.log(comments);
 
@@ -65,7 +66,6 @@ function QuestionDetails() {
 
   type SocketRef = { current: any };
   const socketRef: SocketRef = useRef();
-
 
   useEffect(() => {
     if (!token) {
@@ -76,44 +76,40 @@ function QuestionDetails() {
 
     socketRef.current = io.connect(`${apiUrl}`);
 
-
-    socketRef.current.on('socketId', (id: number) => {
+    socketRef.current.on("socketId", (id: number) => {
       setSocketId(id);
     });
 
-    socketRef.current.on('comment', (commentBody: Comment) => {
-
+    socketRef.current.on("comment", (commentBody: Comment) => {
       console.log(commentBody);
 
       dispatch(saveComment(commentBody));
     });
-
   }, [dispatch]);
 
   const sendComment = (comment: Comment) => (e: any) => {
-    if (e.keyCode === 13 || e.type === 'click') {
+    if (e.keyCode === 13 || e.type === "click") {
       // @ts-ignore
-      socketRef.current.emit('comment', {
+      socketRef.current.emit("comment", {
         questionId: questionId,
         body: comment.body,
         authorId: user.id,
         upVotes: 0,
         isSolution: null,
         author: { firstName: user.firstName, lastName: user.lastName },
-        createdAt: moment(new Date()).format('YYYY-MM-DD, h:mm:ss a'),
+        createdAt: moment(new Date()).format("YYYY-MM-DD, h:mm:ss a"),
       });
       // @ts-ignore
       setComment({
         questionId: questionId,
-        body: '',
+        body: "",
         authorId: user.id,
         upVotes: 0,
         isSolution: null,
-        author: { firstName: '', lastName: '' },
-        createdAt: moment(new Date()).format('YYYY-MM-DD, h:mm:ss a'),
+        author: { firstName: "", lastName: "" },
+        createdAt: moment(new Date()).format("YYYY-MM-DD, h:mm:ss a"),
       });
     }
-
   };
 
   const openScreenshot = () => {
@@ -155,33 +151,39 @@ function QuestionDetails() {
           )}
         </div>
       </div>
-      <div className='comment-section'>
-        <InputGroup className='mb-3 mt-2'>
+      <div className="comment-section">
+        <InputGroup className="mb-3 mt-2">
           <FormControl
             onChange={(e) => setComment({ ...comment, body: e.target.value })}
             onKeyPress={sendComment(comment)}
-            placeholder='write a reply...'
+            placeholder="write a reply..."
             value={comment.body}
-            aria-label='add tag'
-            aria-describedby='basic-addon'
+            aria-label="add tag"
+            aria-describedby="basic-addon"
           />
           <InputGroup.Append>
-            <Button onClick={sendComment(comment)} variant='outline-secondary'>
-              <i className='las la-check'></i>
+            <Button onClick={sendComment(comment)} variant="outline-secondary">
+              <i className="las la-check"></i>
             </Button>
           </InputGroup.Append>
         </InputGroup>
-        <div className='comments'>
+        <div className="comments">
           {comments.map((x: Comment, i: number) => {
             return (
-              <div key={i} className='comment'>
+              <div key={i} className="comment">
                 <h5>{`${x.author.firstName} ${x.author.lastName}`}</h5>
-                <p className='timestamp'>{`${moment(createdAt).calendar()} - ${moment(createdAt)
-                  .startOf('hour')
+                <p className="timestamp">{`${moment(
+                  createdAt
+                ).calendar()} - ${moment(createdAt)
+                  .startOf("hour")
                   .fromNow()}  `}</p>
 
                 <p>{x.body}</p>
-                <UpVotesComments questionId={questionId} upVotes={x.upVotes} commentId={x.id} />
+                <UpVotesComments
+                  questionId={questionId}
+                  upVotes={x.upVotes}
+                  commentId={x.id}
+                />
                 <hr></hr>
               </div>
             );
